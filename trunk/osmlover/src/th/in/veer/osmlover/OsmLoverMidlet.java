b51a835bdd7dd3519cc17dfeb69f06a431b3f95b
vsatayamas <vsatayamas@70e92c5f-826e-a609-6f41-210d59d61080>
2010-08-26 14:31:50 +0000

    first midlet

---
+++ trunk/osmlover/src/th/in/veer/osmlover/OsmLoverMidlet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package th.in.veer.osmlover;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author Vee Satayamas
+ */
+public class OsmLoverMidlet extends MIDlet {
+    public void startApp() {
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+}
